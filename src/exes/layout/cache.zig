35a069cfdcf88d7d7a51ad25f4e0f7d27f790db0
Anthon van der Neut <anthon.van.der.neut@ruamel.eu>
2024-12-24 17:30:44 +0100

    remove extra slash added to alternative
    The extra slash added to the alternative name causes the RSS link on the Zine devlog page to be broken. That link points to a file ( `.../log/index.xml` ) and not a directory.  (`.../log/index.xml/`).
    Some smart testing on whether the `/` is needed, but I IMO this could be inserted by hand if indeed a directory pointing URL is needed.

--- src/exes/layout/cache.zig
+++ src/exes/layout/cache.zig
@@ -968,7 +968,6 @@ fn loadPage(
                                             "/",
                                             site._meta.url_path_prefix,
                                             alt.output,
-                                            "/",
                                         });
                                         break :blk context.String.init(abs);
                                     }
