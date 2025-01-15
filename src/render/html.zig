a4b032df96e5d02092b7a52f72e4a8c6eb0959ef
Alexander Leonhardt <equinox.salexander@gmail.com>
2025-01-15 16:51:44 +0100

    Added the missing directives

--- src/render/html.zig
+++ src/render/html.zig
@@ -93,7 +93,7 @@ pub fn html(
                             }
                             open_div = true;
                             try w.print("<div", .{});
-                            if (d.id) |id| try w.print(" id={s}", .{id});
+                            if (d.id) |id| try w.print(" id=\"{s}\"", .{id});
                             if (d.attrs) |attrs| {
                                 try w.print(" class=\"", .{});
                                 for (attrs) |attr| try w.print("{s} ", .{attr});
@@ -126,7 +126,7 @@ pub fn html(
                         else => {},
                         .heading => {
                             try w.print("<h{}", .{node.headingLevel()});
-                            try w.print(" id={s}", .{d.id.?});
+                            try w.print(" id=\"{s}\"", .{d.id.?});
                             if (d.attrs) |attrs| {
                                 try w.print(" class=\"", .{});
                                 for (attrs) |attr| try w.print("{s} ", .{attr});
@@ -142,7 +142,7 @@ pub fn html(
                             }
                             open_div = true;
                             try w.print("<div", .{});
-                            try w.print(" id={s}", .{d.id.?});
+                            try w.print(" id=\"{s}\"", .{d.id.?});
                             if (d.attrs) |attrs| {
                                 try w.print(" class=\"", .{});
                                 for (attrs) |attr| try w.print("{s} ", .{attr});
